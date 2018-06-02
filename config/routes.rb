a169062c8be729ff00eaa1d36730b718db3198a5
Rodrigo Cavichioli <rodrigoack@gmail.com>
2018-06-02 17:19:24 -0300

    auth route

--- config/routes.rb
+++ config/routes.rb
@@ -8,5 +8,6 @@ Rails.application.routes.draw do
   end
 
   resources :users
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  get '/auth/github', to: 'authentication#github', format: false
 end
